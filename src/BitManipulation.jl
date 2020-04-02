cf9274608b5110b001e311e0bc06cc64322d074d
bouddhaacelee <bouddhaacelee@gmail.com>
2020-04-02 20:34:45 +0200

    Files generated by PkgTemplates

---
+++ src/BitManipulation.jl
@@ -0,0 +1,5 @@
+module BitManipulation
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using BitManipulation
+using Test
+
+@testset "BitManipulation.jl" begin
+    # Write your own tests here.
+end
