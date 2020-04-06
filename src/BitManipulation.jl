d8f2150f32b5b67d975011823192e9c4ce17d174
bouddhaacelee <bouddhaacelee@gmail.com>
2020-04-06 15:30:20 +0200

    transpose array

--- src/BitManipulation.jl
+++ src/BitManipulation.jl
@@ -4,13 +4,13 @@ module BitManipulation
     The result is padded to full bytes by inserting zero bits at the end.
     """
     function packbits(data_to_packs::BitArray, bits_count::Int=8)
-        size_of_data_packed = size(data_to_packs, 2) ÷ bits_count
-        data_packed = zeros(UInt8, size(data_to_packs, 1), size_of_data_packed)
+        size_of_data_packed = size(data_to_packs, 1) ÷ bits_count
+        data_packed = zeros(UInt8, size_of_data_packed, size(data_to_packs, 2))
         for x = 1:size_of_data_packed
-            data_packed[:, x] = data_to_packs[:, 1 + (x-1)*bits_count]
+            data_packed[x, :] = data_to_packs[1 + (x-1)*bits_count, :]
             for i = 2:bits_count
-                data_packed[:, x] .<<= 1
-                data_packed[:, x] .|= data_to_packs[:, i + (x-1)*bits_count]
+                data_packed[x, :] .<<= 1
+                data_packed[x, :] .|= data_to_packs[i + (x-1)*bits_count, :]
             end
         end
         return data_packed
@@ -19,13 +19,15 @@ module BitManipulation
     """
     Packs the 48 elements of a binary-valued array into bits by 6 in a uint8 array.
     """
+
+
     function pack48bitsby6(data_to_packs::BitArray)
-        data_packed = zeros(UInt8, size(data_to_packs, 1), 8)
+        data_packed = zeros(UInt8, 8, size(data_to_packs, 2))
         for x = 1:8
-            data_packed[:, x] = data_to_packs[:, 1 + (x-1)*6]
+            data_packed[x, :] = data_to_packs[1 + (x-1)*6, :]
             for i = 2:6
-                data_packed[:, x] .<<= 1
-                data_packed[:, x] .|= data_to_packs[:, i + (x-1)*6]
+                data_packed[x, :] .<<= 1
+                data_packed[x, :] .|= data_to_packs[i + (x-1)*6, :]
             end
         end
         return data_packed
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,19 +4,17 @@ using Test
 @testset "BitManipulation.jl" begin
     #= only    a byte
     =#
-    data_to_packs = falses(2, 8)
-    data_to_packs[1, :] = BitArray([1, 0, 1, 0, 0, 1, 0, 1]) 
-    data_to_packs[2, :] = BitArray([0, 1, 0, 1, 1, 0, 1, 0])
-
-    expected_result = Array{UInt8, 2}(undef, 2, 1)
-    expected_result[1, :] = [0xa5]
-    expected_result[2, :] = [0x5a]
-
+    data_to_packs = falses(8, 2)
+    data_to_packs[:, 1] = BitArray([1; 0; 1; 0; 0; 1; 0; 1]) 
+    data_to_packs[:, 2] = BitArray([0; 1; 0; 1; 1; 0; 1; 0])
+    expected_result = Array{UInt8, 2}(undef, 1, 2)
+    expected_result[1, 1] = 0xa5
+    expected_result[1, 2] = 0x5a
     @test expected_result == packbits(data_to_packs)
     
-    data_to_packs = falses(2, 16)
-    data_to_packs[1, :] = BitArray([1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0]) 
-    data_to_packs[2, :] = BitArray([0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1]) 
+    data_to_packs = falses(16, 2)
+    data_to_packs[:, 1] = BitArray([1; 0; 1; 0; 0; 1; 0; 1; 0; 1; 0; 1; 1; 0; 1; 0]) 
+    data_to_packs[:, 2] = BitArray([0; 1; 0; 1; 1; 0; 1; 0; 1; 0; 1; 0; 0; 1; 0; 1]) 
     
     expected_result = Array{UInt8, 2}(undef, 2, 2)
     expected_result[1, :] = [0xa5, 0x5a]
@@ -24,56 +22,56 @@ using Test
 
     @test expected_result == packbits(data_to_packs)
     
-    data_to_packs = falses(4, 32)
-    data_to_packs[1, :] = BitArray([1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0]) 
-    data_to_packs[2, :] = BitArray([0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1]) 
-    data_to_packs[3, :] = BitArray([1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0]) 
-    data_to_packs[4, :] = BitArray([0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1]) 
+    data_to_packs = falses(32, 4)
+    data_to_packs[:, 1] = BitArray([1; 0; 1; 0; 0; 1; 0; 1; 0; 1; 0; 1; 1; 0; 1; 0; 1; 0; 1; 0; 0; 1; 0; 1; 0; 1; 0; 1; 1; 0; 1; 0]) 
+    data_to_packs[:, 2] = BitArray([0; 1; 0; 1; 1; 0; 1; 0; 1; 0; 1; 0; 0; 1; 0; 1; 0; 1; 0; 1; 1; 0; 1; 0; 1; 0; 1; 0; 0; 1; 0; 1]) 
+    data_to_packs[:, 3] = BitArray([1; 0; 1; 0; 0; 1; 0; 1; 0; 1; 0; 1; 1; 0; 1; 0; 1; 0; 1; 0; 0; 1; 0; 1; 0; 1; 0; 1; 1; 0; 1; 0]) 
+    data_to_packs[:, 4] = BitArray([0; 1; 0; 1; 1; 0; 1; 0; 1; 0; 1; 0; 0; 1; 0; 1; 0; 1; 0; 1; 1; 0; 1; 0; 1; 0; 1; 0; 0; 1; 0; 1]) 
     
     expected_result = Array{UInt8, 2}(undef, 4, 4)
-    expected_result[1, :] = [0xa5, 0x5a, 0xa5, 0x5a]
-    expected_result[2, :] = [0x5a, 0xa5, 0x5a, 0xa5]
-    expected_result[3, :] = [0xa5, 0x5a, 0xa5, 0x5a]
-    expected_result[4, :] = [0x5a, 0xa5, 0x5a, 0xa5]
+    expected_result[:, 1] = [0xa5, 0x5a, 0xa5, 0x5a]
+    expected_result[:, 2] = [0x5a, 0xa5, 0x5a, 0xa5]
+    expected_result[:, 3] = [0xa5, 0x5a, 0xa5, 0x5a]
+    expected_result[:, 4] = [0x5a, 0xa5, 0x5a, 0xa5]
     @test expected_result == packbits(data_to_packs)
 
-    data_to_packs = falses(2, 6)
-    data_to_packs[1, :] = BitArray([1, 0, 0, 1, 0, 1]) 
-    data_to_packs[2, :] = BitArray([0, 1, 1, 0, 1, 0])
+    data_to_packs = falses(6, 2)
+    data_to_packs[:, 1] = BitArray([1, 0, 0, 1, 0, 1]) 
+    data_to_packs[:, 2] = BitArray([0, 1, 1, 0, 1, 0])
 
-    expected_result = Array{UInt8, 2}(undef, 2, 1)
-    expected_result[1, :] = [0x25]
-    expected_result[2, :] = [0x1a]
+    expected_result = Array{UInt8, 2}(undef, 1, 2)
+    expected_result[:, 1] = [0x25]
+    expected_result[:, 2] = [0x1a]
 
     @test expected_result == packbits(data_to_packs, 6)
 
-    data_to_packs = falses(2, 12)
-    data_to_packs[1, :] = BitArray([1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1]) 
-    data_to_packs[2, :] = BitArray([0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0])
+    data_to_packs = falses(12, 2)
+    data_to_packs[:, 1] = BitArray([1; 0; 0; 1; 0; 1; 1; 0; 0; 1; 0; 1]) 
+    data_to_packs[:, 2] = BitArray([0; 1; 1; 0; 1; 0; 0; 1; 1; 0; 1; 0])
 
     expected_result = Array{UInt8, 2}(undef, 2, 2)
-    expected_result[1, :] = [0x25, 0x25]
-    expected_result[2, :] = [0x1a, 0x1a]
+    expected_result[:, 1] = [0x25, 0x25]
+    expected_result[:, 2] = [0x1a, 0x1a]
 
     @test expected_result == packbits(data_to_packs, 6)
 
-    data_to_packs = falses(2, 24)
-    data_to_packs[1, :] = BitArray([1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1]) 
-    data_to_packs[2, :] = BitArray([0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0])
+    data_to_packs = falses(24, 2)
+    data_to_packs[:, 1] = BitArray([1; 0; 0; 1; 0; 1; 1; 0; 0; 1; 0; 1; 1; 0; 0; 1; 0; 1; 1; 0; 0; 1; 0; 1]) 
+    data_to_packs[:, 2] = BitArray([0; 1; 1; 0; 1; 0; 0; 1; 1; 0; 1; 0; 0; 1; 1; 0; 1; 0; 0; 1; 1; 0; 1; 0])
 
-    expected_result = Array{UInt8, 2}(undef, 2, 4)
-    expected_result[1, :] = [0x25, 0x25, 0x25, 0x25]
-    expected_result[2, :] = [0x1a, 0x1a, 0x1a, 0x1a]
+    expected_result = Array{UInt8, 2}(undef, 4, 2)
+    expected_result[:, 1] = [0x25; 0x25; 0x25; 0x25]
+    expected_result[:, 2] = [0x1a; 0x1a; 0x1a; 0x1a]
 
     @test expected_result == packbits(data_to_packs, 6)
 
-    data_to_packs = falses(2, 48)
-    data_to_packs[1, :] = BitArray([1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1]) 
-    data_to_packs[2, :] = BitArray([0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0])
+    data_to_packs = falses(48, 2)
+    data_to_packs[:, 1] = BitArray([1; 0; 0; 1; 0; 1; 1; 0; 0; 1; 0; 1; 1; 0; 0; 1; 0; 1; 1; 0; 0; 1; 0; 1; 1; 0; 0; 1; 0; 1; 1; 0; 0; 1; 0; 1; 1; 0; 0; 1; 0; 1; 1; 0; 0; 1; 0; 1]) 
+    data_to_packs[:, 2] = BitArray([0; 1; 1; 0; 1; 0; 0; 1; 1; 0; 1; 0; 0; 1; 1; 0; 1; 0; 0; 1; 1; 0; 1; 0; 0; 1; 1; 0; 1; 0; 0; 1; 1; 0; 1; 0; 0; 1; 1; 0; 1; 0; 0; 1; 1; 0; 1; 0])
 
-    expected_result = Array{UInt8, 2}(undef, 2, 8)
-    expected_result[1, :] = [0x25, 0x25, 0x25, 0x25, 0x25, 0x25, 0x25, 0x25]
-    expected_result[2, :] = [0x1a, 0x1a, 0x1a, 0x1a, 0x1a, 0x1a, 0x1a, 0x1a]
+    expected_result = Array{UInt8, 2}(undef, 8, 2)
+    expected_result[:, 1] = [0x25; 0x25; 0x25; 0x25; 0x25; 0x25; 0x25; 0x25]
+    expected_result[:, 2] = [0x1a; 0x1a; 0x1a; 0x1a; 0x1a; 0x1a; 0x1a; 0x1a]
 
     @test expected_result == pack48bitsby6(data_to_packs)
 
